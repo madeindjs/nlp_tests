db10feb6125379bf63c5de4e9554322f1f7f59e4
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-02-19 12:04:57 +0100

    [+] Parse first page

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
+
+class Website():
+    """Represent a Website
+    """
+
+    def __init__(self, root_url):
+        self.root_url = root_url
+        self.page = []
+
+    def crawl(self):
+        first_page = Page(self.root_url, self.root_url)
+        print([l for l in first_page.get_links()])
+
+
+
+class Page():
+
+    def __init__(self, url, root_url):
+        self.url = url
+        self.root_url = root_url
+        self._initialise_soup()
+
+    def get_links(self, mode = 'all'):
+        for link in self.soup.findAll('a', attrs={'href': re.compile("^/")}):
+            yield self.root_url + link.get('href')
+
+    def _initialise_soup(self):
+        r = requests.get(self.url)
+        self.soup = BeautifulSoup(r.text, 'html.parser')
+
+
+
+
 def main():
-    r = requests.get('http://rousseau-alexandre.fr')
-    soup = BeautifulSoup(r.text, 'html.parser')
-    print(soup.prettify())
+    w = Website('http://rousseau-alexandre.fr')
+    w.crawl()
 
 if __name__ == '__main__':
     main()
