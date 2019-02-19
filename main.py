d9f8a4acb01618489cfe272e7bece04b101b206f
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-02-19 14:30:45 +0100

    [M] Pasre all links

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import logging
 
 
 class Website():
-    """Represent a Website
-    """
 
     def __init__(self, root_url):
         self.root_url = root_url
-        self.page = []
+        self.pages_to_crawl = []
+        self.url_crawled = []
+        self.pages_to_crawl.append( Page(self.root_url) )
 
     def crawl(self):
-        first_page = Page(self.root_url, self.root_url)
-        print([l for l in first_page.get_links()])
-
+        """Get the root page and start to walk between them"""
+        while self.pages_to_crawl:
+            page = self.pages_to_crawl.pop()
+            # print(page.links)
+            for link in page.links:
+                if link not in self.url_crawled:
+                    p = Page(self.root_url + link)
+                    self.pages_to_crawl.append(p)
+                    self.url_crawled.append(link)
 
 
 class Page():
 
-    def __init__(self, url, root_url):
+    @staticmethod
+    def get_soup(url):
+        """Build a Beautifull soup object from request result"""
+        r = requests.get(url)
+        return BeautifulSoup(r.text, 'html.parser')
+
+    def __init__(self, url):
+        """Initialize page and make a GET query"""
+        # logger = logging.getLogger("nom_programme")
+        # logger.info('Start crawling %s', url)
+        print('Start crawling ' + url)
         self.url = url
-        self.root_url = root_url
-        self._initialise_soup()
+        self.soup = Page.get_soup(self.url)
+        self.links = self._get_links()
+        # self.links = [l for l in self.get_links()]
 
-    def get_links(self, mode = 'all'):
+    def _get_links(self, mode = 'all'):
+        """Parse content and get all links"""
         for link in self.soup.findAll('a', attrs={'href': re.compile("^/")}):
-            yield self.root_url + link.get('href')
-
-    def _initialise_soup(self):
-        r = requests.get(self.url)
-        self.soup = BeautifulSoup(r.text, 'html.parser')
-
+            yield link.get('href')
 
 
 
